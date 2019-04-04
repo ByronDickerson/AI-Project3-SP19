728da4ff6ec28334689873af2891ba954646bc71
Olivia Lyons <32911243+ojlyons@users.noreply.github.com>
2019-04-04 19:13:20 -0400

    Update Factory.py

--- BATTLECODE/My Player/Factory.py
+++ BATTLECODE/My Player/Factory.py
@@ -5,7 +5,7 @@ possibleDirections = list(bc.Direction)
 
 
 def factoryLogic(unit, gc):
-   
+    
     counter = [1, 2, 3]
     count = random.choice(counter)
     if not unit.structure_is_built():
@@ -74,23 +74,82 @@ def launch(gc, marsMap, unitId):
 	if gc.can_launch_rocket(this_rocket.id, destination):
 		gc.launch_rocket(this_rocket.id, destination)
 
+#basically jsut to hold some paramebers to go by
+class Worker:
+    def __init__(self, id):
+        self.ID = id
+        self.isBuilding = False
+        self.isImproved = False
+        self.buildTargetID = None
+        
+    
+    def newBuildTarget(self,tgt):
+        self.buildTargetID = tgt.id
+        self.isBuilding = True
+
+
+#input 'worker' is of the type Worker as defined in this file.
+def workerLogic(gc, worker, info):
+    
+    #The worker will prioritize building
+    #Then attacking
+    #THen gathering resources
+    #Then replicating
+    #Then wandering around
+    if worker.isBuilding:
+        if gc.can_build(worker.ID, worker.buildTargetID):
+            gc.build(worker.ID, worker.buildTargetID)
+            return
+    
+    location = worker.location
+    my_team = gc.team()
+
+    #attack nearby enemy
+    #should probably have it flee but that is much harder to computer
+    if location.is_on_map():
+        nearby = gc.sense_nearby_units(location.map_location(), 2)
+        for other in nearby:
+            if other.team != my_team and gc.is_attack_ready(worker.ID) and gc.can_attack(worker.ID, other.id):
+                print(worker.ID, 'attacking',other.id)
+                gc.attack(worker.ID, other.id)
+                return
+                
+    directions = list(bc.Direction)
+    #d = random.choice(directions)
+    
+    for d in directions:
+        if gc.can_harvest(worker.ID, d):
+            gc.harvest(worker.ID, d)
+            return
+    
+    for d in directions:
+        if gc.can_replicate(worker.ID, d):
+            gc.replicate(worker.ID, d)
+            # a child is born. we should initialize it as a Worker class. but...for now...whatever man
+    
+    #check if we can blueprint...then...do it..
+    #this method doesnt exist yet. but it should handle like do we need one, do we have the funds, etc
+    
+    if info.isRocketBlueprintTime():
+        blueprint(worker,directions,location,bc.UnitType.Rocket)
+        return
+        
+    if info.isFactoryBlueprintTime():
+        blueprint(worker,directions,location,utype)
+        return
+        
     
     
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+def blueprint(worker,directions,location,utype):
+    for d in directions:
+            if gc.can_blueprint(worker.ID, utype,d):
+                gc.blueprint(worker.ID, utype,d)
+                for u in gc.sense_nearby_units(location.map_location()):
+                    if u.unit_type == utype:
+                        worker.newBuildTarget(u)
+                return
     
     
     
